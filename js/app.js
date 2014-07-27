578dae66a89516846130d53e262e484c66ba31cc
Makoto Ohnami <z.ohnami@gmail.com>
2014-07-27 14:18:13 +0900

    adjusted view style.

--- js/app.js
+++ js/app.js
@@ -18,28 +18,65 @@ var demo = new Vue({
     el: 'body',
     data: {
         title: 'Title',
+        currentTargetNo: 1,
         currentView: 'form-weather',
-        todos: [
+        targetList: [
             {
-                done: true,
-                content: 'Learn JavaScript'
+                targetNo: 1,
+                targetName: 'target1',
+                targetHost: 'http://www.yahoo.co.jp'
             },
             {
+                targetNo: 2,
+                targetName: 'target2',
+                targetHost: 'http://www.yahoo.co.jp'
+            },
+            {
+                targetNo: 3,
+                targetName: 'target3',
+                targetHost: 'http://www.yahoo.co.jp'
+            }
+        ],
+        todos: [
+            {
+                no: 1,
+                done: true,
+                content: 'function1',
+                contentView: 'form-weather'
+            },
+            {
+                no: 2,
                 done: false,
-                content: 'Learn Vue.js'
+                content: 'function2',
+                contentView: 'form-sub'
             }
         ]
     },
-    methods: {
-        change: function (e) {
-            // console.log(e.target.tagName) // "A"
-            // console.log(e.targetVM === this) // true
-            if(this.currentView == 'form-weather') {
-                this.currentView = 'form-sub';
-            } else {
-                this.currentView = 'form-weather';
+    computed: {
+        currentTargetName: function() {
+            var l = this.targetList.length;
+            for(var i = 0; i < l; i++) {
+                if(this.currentTargetNo == this.targetList[i].targetNo) {
+                    return this.targetList[i].targetName;
+                }
             }
-            API('ppp');
+            return null;
+        }
+    },
+    methods: {
+        changeView: function(no) {
+            var l = this.todos.length;
+            for(var i = 0; i < l; i++) {
+                if(no == this.todos[i].no) {
+                    this.todos[i].done = true;
+                    this.currentView = this.todos[i].contentView;
+                } else {
+                    this.todos[i].done = false;
+                }
+            }
+        },
+        changeTarget: function (targetNo) {
+            this.currentTargetNo = targetNo;
         },
         getWeather: function (e) {
           $.ajax({
@@ -56,23 +93,4 @@ var demo = new Vue({
     }
 });
 
-
-// var app = new Vue({
-//     el: '#app',
-//     data: {
-//         currentView: 'home'
-//     },
-//     methods: {
-//         change: function (e) {
-//             // console.log(e.target.tagName) // "A"
-//             // console.log(e.targetVM === this) // true
-//             if(this.currentView == 'home') {
-//                 this.currentView = 'page1';
-//             } else {
-//                 this.currentView = 'home';
-//             }
-//             API('ppp');
-//         }
-//     }
-// });
 
